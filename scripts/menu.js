1dd9e15a7fbf2c35077b90bed17c5dfd47be976e
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-05-11 23:15:09 -0500

    created the base index,menu and js pages

---
+++ scripts/menu.js
@@ -0,0 +1,28 @@
+"use strict"
+console.log("Hello Human");
+
+let menu = {
+drinks : [
+    "Water", "Tea", "Sweet Tea",
+    "Coke", "Dr. Pepper", "Sprite"
+    ],
+    entrees : [
+    "Hamburger w/ Fries",
+    "Grilled Cheese w/ Tater Tots",
+    "Grilled Chicken w/ Veggies",
+    "Chicken Fried Steak w/ Mashed Potatoes",
+    "Fried Shrimp w/ Coleslaw",
+    "Veggie Plate"
+    ],
+    desserts: [
+    "Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"
+    ]
+    };
+
+    //creating a onload to run once the page is loaded
+    window.onload = function(){
+
+        initDropdown()
+
+
+    }
